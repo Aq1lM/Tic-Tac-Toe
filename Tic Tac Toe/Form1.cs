d1d646c8a94c3e80b2e1d54ed225ab24c4841a8d
Aqil Məmmədov <memmedovaqil202@gmail.com>
2021-09-13 16:15:39 +0400

    Add project files.

---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        char who = 'x';
+        int movement = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            who = 'x';
+            movement = 0;
+            b1.Enabled = true; b1.Text = ""; b1.BackColor = Color.White;
+            b2.Enabled = true; b2.Text = ""; b2.BackColor = Color.White;
+            b3.Enabled = true; b3.Text = ""; b3.BackColor = Color.White;
+            b4.Enabled = true; b4.Text = ""; b4.BackColor = Color.White;
+            b5.Enabled = true; b5.Text = ""; b5.BackColor = Color.White;
+            b6.Enabled = true; b6.Text = ""; b6.BackColor = Color.White;
+            b7.Enabled = true; b7.Text = ""; b7.BackColor = Color.White;
+            b8.Enabled = true; b8.Text = ""; b8.BackColor = Color.White;
+            b9.Enabled = true; b9.Text = ""; b9.BackColor = Color.White;
+            tableLayoutPanel1.Enabled = true;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            bt.Enabled = false;
+            bt.BackColor = Color.DarkGray;
+            if (who == 'x')
+            {
+                bt.Text = "x";
+                if ((b1.Text == b2.Text && b2.Text == b3.Text && b1.Text != "") ||
+                    (b4.Text == b5.Text && b5.Text == b6.Text && b4.Text != "") ||
+                    (b7.Text == b8.Text && b8.Text == b9.Text && b7.Text != "") ||
+                    (b1.Text == b4.Text && b4.Text == b7.Text && b1.Text != "") ||
+                    (b2.Text == b5.Text && b5.Text == b8.Text && b2.Text != "") ||
+                    (b3.Text == b6.Text && b6.Text == b9.Text && b3.Text != "") ||
+                    (b1.Text == b5.Text && b5.Text == b9.Text && b1.Text != "") ||
+                    (b3.Text == b5.Text && b5.Text == b7.Text && b3.Text != ""))
+                {
+                    MessageBox.Show($"The Winner is {who.ToString().ToUpper()} !");
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if (movement == 8)
+                {
+                    MessageBox.Show("DRAW!");
+                }
+                who = 'o';
+            }
+            else if (who == 'o')
+            {
+                bt.Text = "o";
+                if ((b1.Text == b2.Text && b2.Text == b3.Text && b1.Text != "") ||
+                    (b4.Text == b5.Text && b5.Text == b6.Text && b4.Text != "") ||
+                    (b7.Text == b8.Text && b8.Text == b9.Text && b7.Text != "") ||
+                    (b1.Text == b4.Text && b4.Text == b7.Text && b1.Text != "") ||
+                    (b2.Text == b5.Text && b5.Text == b8.Text && b2.Text != "") ||
+                    (b3.Text == b6.Text && b6.Text == b9.Text && b3.Text != "") ||
+                    (b1.Text == b5.Text && b5.Text == b9.Text && b1.Text != "") ||
+                    (b3.Text == b5.Text && b5.Text == b7.Text && b3.Text != ""))
+                {
+                    MessageBox.Show($"The Winner is {who.ToString().ToUpper()} !");
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if (movement == 8)
+                {
+                    MessageBox.Show("DRAW!");
+                }
+                who = 'x';
+            }
+
+            movement++;
+        }
+    }
+}
